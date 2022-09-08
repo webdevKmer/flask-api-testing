b72a435b4a205dc58d3e43013e789e2ceabeecde
webdevKmer <webdevKmer@gmail.com>
2022-09-08 21:37:07 +0000

    fetching some apis...

---
+++ flask_fetch_apis.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+url_users = "https://jsonplaceholder.typicode.com/users"
+url_posts = "https://jsonplaceholder.typicode.com/posts"
+url_albums = "https://jsonplaceholder.typicode.com/albums"
+url_todos = "https://jsonplaceholder.typicode.com/todos"
+url_comments = "https://jsonplaceholder.typicode.com/comments"
+url_photos = "https://jsonplaceholder.typicode.com/photos"
+
+def get_data(url):
+    response = json.loads(requests.request("GET", url).text)
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/users")
+def get_users():
+    users = get_data(url_users)
+    return render_template("users.html", users=users)
+
+@app.route("/posts")
+def get_posts():
+    posts = get_data(url_posts)
+    return render_template("posts.html", posts=posts)
+
+app.run(debug=True, host="0.0.0.0", port=8080)
---
+++ flask_render.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(host="0.0.0.0", port=8080)
