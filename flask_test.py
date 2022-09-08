77c2b503ba94238fe0e2ee5a9170bd48e1bcaf24
webdevKmer <webdevKmer@gmail.com>
2022-09-08 19:51:06 +0000

    spinning the first flask app.

--- flask_test.py
+++ flask_test.py
@@ -1,6 +1,13 @@
+#!/usr/bin/python3
+
 from flask import Flask
 import requests
 import json
 
-app Flask(__name__)
+app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return "Hello from Flask"
+
+app.run(host="0.0.0.0", port=8080)
