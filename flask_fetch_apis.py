d988c181fce2f26c077aa5874c463e0023445555
webdevKmer <webdevKmer@gmail.com>
2022-09-08 22:42:04 +0000

    other changes

--- flask_fetch_apis.py
+++ flask_fetch_apis.py
@@ -30,4 +30,29 @@ def get_posts():
     posts = get_data(url_posts)
     return render_template("posts.html", posts=posts)
 
+@app.route("/albums")
+def get_albums():
+    albums = get_data(url_albums)
+    return render_template("albums.html", albums=albums)
+
+
+@app.route("/todos")
+def get_todos():
+    todos = get_data(url_todos)
+    nbr_todos = len(todos)
+    return render_template("todos.html", todos=todos, nbr_todos=nbr_todos)
+
+
+@app.route("/comments")
+def get_comments():
+    comments = get_data(url_comments)
+    nbr_comments = len(comments)
+    return render_template("comments.html", comments=comments, nbr_comments=nbr_comments)
+
+
+@app.route("/photos")
+def get_photos():
+    photos = get_data(url_photos)
+    nbr_photos = len(photos)
+    return render_template("photos.html", photos=photos, nbr_photos=nbr_photos)
 app.run(debug=True, host="0.0.0.0", port=8080)
